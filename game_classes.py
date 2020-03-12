9d2653eec51631e119bf74c814e9f3f3e0f4f09b
adak-crypto <ada.kochlewska@gmail.com>
2020-03-12 18:37:01 +0100

    calibration

--- game_classes.py
+++ game_classes.py
@@ -14,8 +14,9 @@ from utils import *
 def debug():
     import ipdb; ipdb.set_trace()
 
+
 class Simple_Game(object):
-    """Simple_Game"""
+    """dSimple_Game"""
     def __init__(self, size,):
         pygame.init()
         try:
@@ -23,7 +24,7 @@ class Simple_Game(object):
         except ValueError as e:
             print('123', e)
             exit(1)
-		
+
         self.bgcolour = 0x2F, 0x4F, 0x4F  # darkslategrey   
         self.size = size
         self.max_lifes = 10
@@ -38,9 +39,8 @@ class Simple_Game(object):
         self.clock = pygame.time.Clock()
 
         self.backgrounds = sorted([x for x in get_backgrounds()])
-        self.backgrounds = [pygame.image.load(x) for x in self.backgrounds]
+        self.backgrounds = [pygame.image.load(x) for x in 	self.backgrounds]
         self.max_shift = 10
-
 
     def move_thrash(self, arg):
         if not self.trash:
@@ -52,7 +52,7 @@ class Simple_Game(object):
     def main(self):
         play = True
 
-        # while play:
+        #while play:
         play = self.menu()
 
     def muscle_move(self, x):
@@ -65,43 +65,86 @@ class Simple_Game(object):
         elif x >= self.calib_max:
             return 1
         else:
-            if x - self.calib_min <= d:
-                return - 0.5
-            elif d < x - self.calib_min <= 2 * d:
+            s = x - self.calib_min
+            if s <= d:
+                return - ( 1 - min(s/d,1) )
+            elif d < s <= 2 * d:
                 return 0
-            elif d * 2 < x - self.calib_min <= 3 * d:
-                return  0.5
+            elif d * 2 < s <= 3 * d:
+	            return  min((s - d * 2)/d, 1)
 
 
     def calibrate(self):
+
+        self.bgcolour = (232,98,203)
+        
+        #self.screen.fill(self.bgcolour)
+        x_screen, y_screen = self.size
+
+        display_surface = pygame.display.set_mode((x_screen, y_screen))
+        display_surface.fill(self.bgcolour)
+        font = pygame.font.Font('freesansbold.ttf', 70)
+        text = font.render('Kalibracja', True, (255,239,148)) 
+        textRect = text.get_rect()
+        textRect.center = (x_screen // 2, y_screen // 2)
+        display_surface.blit(text, textRect)
+
+        pygame.display.update()
+
         print("kalibracja")
 		
         time.sleep(2)
+
+        display_surface.fill(self.bgcolour)
+        text = font.render('Rozluźnij rękę', True, (255,239,148)) 
+        textRect = text.get_rect()
+        textRect.center = (x_screen // 2, y_screen // 2)
+        display_surface.blit(text, textRect)
+        pygame.display.update()
+
         print("rozluznij reke")
         time.sleep(1)
         self.calib_min = self.amp.calib()
         # self.calib_min = 10
         time.sleep(2)
+
+        display_surface.fill(self.bgcolour)
+        text = font.render('Zaciśnij rękę', True, (255,239,148)) 
+        textRect = text.get_rect()
+        textRect.center = (x_screen // 2, y_screen // 2)
+        display_surface.blit(text, textRect)
+        pygame.display.update()
+
         print("zacisnij reke")
         time.sleep(1)
         self.calib_max = self.amp.calib()
         # self.calib_max = 400
         if self.calib_min >= self.calib_max or  self.calib_max - self.calib_min < 50:
+            display_surface.fill(self.bgcolour)
+            text = font.render('Powtarzam kalibrację', True, (255,239,148)) 
+            textRect = text.get_rect()
+            textRect.center = (x_screen // 2, y_screen // 2)
+            display_surface.blit(text, textRect)
+            pygame.display.update()
             print("powtarzam kalibrację")
             self.calibrate()
+        self.play()
         
         			
-			
+
 
     def play(self):
-        self.calibrate()
+        #self.calibrate()
+        #debug()
         self.amp.amp.start_sampling()        
         time.sleep(1)	
 		
         self.lifes = self.max_lifes
         self.score = 0 
         self.missed = 0 
-        self.background = None
+        #self.background = None
+        self.backgrounds = sorted([x for x in get_backgrounds()])
+        self.backgrounds = [pygame.image.load(x) for x in 	self.backgrounds]
         self.bgn_idx = 0
         speed_rate = 0.003
 
@@ -200,12 +243,30 @@ class Simple_Game(object):
                 #     pygame.quit()
 
                 # ploting stuff
+                x_screen, y_screen = self.size
+                x_button = 100
+                y_button = 50
+                button_colour = (255,239,148)
+                text_colour = (232,98,203)
+
                 self.screen.fill(self.bgcolour)
                 self.update_background()
                 for bin_ in bins:
                     self.screen.blit(bin_.image, bin_.pos)
                 self.screen.blit(self.trash.image, self.trash.pos)
-                pygame.display.update()
+                b_m = Button(self.screen, 'Menu', (x_button/2,y_button/2), (x_button, y_button), button_colour, text_colour, self.menu)
+                #pygame.display.update(b_m)
+                
+                while True:
+                    for event in pygame.event.get():
+                        b_m.onClick(event)
+                        if event.type == pygame.QUIT:
+                            pygame.quit(); sys.exit();
+                        if event.type == pygame.KEYDOWN:
+                            if event.key == pygame.K_ESCAPE:
+                                pygame.quit(); sys.exit();
+                pygame.display.update(b_m)
+                
 
                 self.clock.tick(60)
                 
@@ -223,24 +284,10 @@ class Simple_Game(object):
         idx = int(idx)
 
         idx = min(idx, len(self.backgrounds)-1)
+
         img = self.backgrounds[idx]
         self.screen.blit(img,[0,0])
-
-
-    # def update_background(self):
-        
-    #     x = (self.max_lifes + self.max_missed) / (len(self.backgrounds) - 1)
-    #     idx = (len(self.backgrounds) - 1 - (self.max_lifes - self.lifes + self.max_missed - min(self.max_missed,self.missed)) // x)
-    #     idx = int(idx)
-    #     print(x, 
-    #         (self.max_lifes + self.max_missed) / (len(self.backgrounds) - 1),
-    #         (len(self.backgrounds) - 1 - (self.max_lifes - self.lifes + self.max_missed - min(self.max_missed,self.missed)) // x)            
-    #         )
-    #     idx = min(idx, len(self.backgrounds))
-    #     img = self.backgrounds[idx]
-    #     self.screen.blit(img,[0,0])
-
-
+    
     @property
     def can_stear(self, obj):
         if self.allow_stearing:
@@ -249,54 +296,204 @@ class Simple_Game(object):
 
     def menu(self):
         # remove when menu is finished
-        self.play()
-        print('done')
+        #self.play()
+        #print('done')
+        
+        self.bgcolour = (232,98,203)
+        
+        self.screen.fill(self.bgcolour)
+        
+        pygame.display.set_caption('Segreguj smieci')
+        #screen = pygame.display.set_mode((600,400), 0, 32)
+        #screen.fill((154, 7, 118))
+        
+        x_screen, y_screen = self.size
+        x_button = 300
+        y_button = 150
+        button_colour = (255,239,148) #(237, 168, 19) #(173, 170, 218)
+        text_colour = (232,98,203) #(122, 73, 122) #(218, 170, 214)
+        
+        #(self, screen, label, pos, dims, button_color, label_color=(255,255,255))
+    
+        b_s = Button(self.screen, 'Start', (x_screen/2,y_screen/2-1.5*y_button), (x_button, y_button), button_colour, text_colour, self.calibrate) #self.play)
+        b_w = Button(self.screen, 'Wyniki', (x_screen/2,y_screen/2), (x_button, y_button), button_colour, text_colour, self.game_score)
+        b_e = Button(self.screen, 'Wyjdź', (x_screen/2,y_screen/2+1.5*y_button), (x_button, y_button), button_colour, text_colour, pygame.quit)
+ 
+        pygame.display.update()
+        while True:
+            for event in pygame.event.get():
+                b_s.onClick(event)
+                b_w.onClick(event)
+                b_e.onClick(event)
+                if event.type == pygame.QUIT:
+                    pygame.quit(); sys.exit();
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit(); sys.exit();
+        pygame.display.update()
 
         return False
 
 
+##### wersja bez mięśni######################################## 			
+'''
+    def play(self):
 
-# buttonText = pygame.font.SysFont('', 20)
- 
+        lifes = 10
+        self.score = 0 
+        speed_rate = 0.0003
+
+
+        w, h = self.size
+
+        bins = [TrashBin(width=w*TrashBin.precent, img_path=path, type=type) for (type, path) in get_bins()]
+
+        number_of_bins = len(bins)
+        bin_widht = w * bins[0].precent
+
+        offset = ( 1 - number_of_bins * TrashBin.precent ) / ( number_of_bins + 1 )
+        offset = round( w * offset )
+
+        
+
+        bin_w, bin_h = bin_size = bins[0].size
+
+        pos_y = h - bin_h
+
+        for i, bin_ in enumerate(bins):
+            pos_x = bin_w * i
+            pos_x += offset * (i + 1)
+            bin_.x = pos_x
+            bin_.y = pos_y
+
+        trashes = [Trash(width=w*Trash.precent, img_path=path, type=type) for (type, path) in get_trashes()] 
+        # debug()
+        play = True
+        new_trash = True
+        print(bool(trashes))
+        while trashes and play and lifes > 0:
+            print("while")
+
+            if new_trash:
+                print("new trash")
+                trash = trashes.pop()
+                # recalcualte x to be in center
+                trash.x = w//2 - trash.size[1]//2
+                trash.y = 100
+                new_trash = False
+                this_trash = True
+
+            if not lifes:
+                play = False
+
+            while this_trash:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        play = False
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_ESCAPE:
+                            play = False
+
+                        if event.key == pygame.K_LEFT:
+                            trash.x += -10
+
+                        if event.key == pygame.K_RIGHT:
+                            trash.x += 10
+
+                trash_x, trash_y = trash.pos 
+
+                translation_y = w * speed_rate
+                trash.x, trash.y = trash_x, trash_y +  translation_y
+
+                if trash.bottom > pos_y:
+
+                    for (i, bin_) in enumerate(bins):
+                        # if bin_.image.colliderect(trash.image):
+                        #     print(i)
+                        if collide_in(trash, bin_):
+
+                            if bin_.type == trash.type:
+                                print('same')
+
+                                self.score += 100
+                            else:
+                                print("wrong")
+                                self.score += 10   
+                            print(self.score)
+
+                        else:
+                            print("miss")
+
+                            lifes -= 1
+                            print(lifes, 'lifes left')
+                    new_trash = True
+                    this_trash = False
+
+
+
+                # if trash.top > self.size[1]:
+                #     pygame.quit()
+
+                # ploting stuff
+                self.screen.fill(self.bgcolour)
+                for bin_ in bins:
+                    self.screen.blit(bin_.image, bin_.pos)
+                self.screen.blit(trash.image, trash.pos)
+                pygame.display.update()
+
+
+                self.clock.tick(60)
+'''
+
+#################################################################
+
+
 class Button(object):
-    def __init__ (self, colour, x, y, width, height, label):
-        self.__colour = colour
-        self.__x = x
-        self.__y = y
-        self.__width = width
-        self.__height = height
-        self.__label = label
- 
-    @property
-    def colour(self):
-        return self.colour
- 
-    @property
-    def x(self):
-        return self.x
- 
-    @property
-    def y(self):
-        return self.y
-
-    @property
-    def width(self):
-        return self.width
- 
-    @property
-    def height(self):
-        return self.height
- 
-    @property
-    def label(self):
-        return self.label
+    def __init__(self, screen, label, pos, dims, button_color, label_color, func):
+        
+        self.screen = screen
+        self.label = label
+        self.pos = pos
+        self.dims = dims
+        self.button_color = button_color
+        self.label_color = label_color
+        self.font = pygame.font.SysFont('Arial', 25)
+        self.func = func
+        
+        
+        self.addRect()
+        self.addText()
 
 
-    # def draw(self, surface):
-    #     self._render()
-    #     surface.blit(self._surface, self._rect.topleft)
+
+    def addRect(self):
+        pos_x, pos_y = self.pos
+        weight, height = self.dims
+        self.rect = pygame.draw.rect(self.screen, self.button_color, (pos_x-weight/2, pos_y-height/2, weight, height), 0)
 
 
+    def addText(self):
+        l = self.label
+        pos_x, pos_y = self.pos
+        weight, height = self.dims
+        text_width, text_height = self.font.size(l)
+        self.screen.blit(self.font.render(l, True, self.label_color), (pos_x-text_width/2, pos_y-text_height/2))
+        
+        
+    def onClick(self, event):
+        
+        func = self.func
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = event.pos  # gets mouse position
+
+            # checks if mouse position is over the button
+
+            if self.rect.collidepoint(mouse_pos):
+                # prints current location of mouse
+                #print('button was pressed at {0}'.format(mouse_pos))
+                func()
+                
 class Trash(pygame.sprite.Sprite):
     """docstring for Trash"""
 
@@ -306,7 +503,7 @@ class Trash(pygame.sprite.Sprite):
 
         pygame.sprite.Sprite.__init__(self)
         self.type = type
-        self.image = pygame.image.load(img_path).convert()
+        self.image = pygame.image.load("static/trash/plastic_metal/blank-shampoo-bottle-1415298.png").convert()
         self.image.set_colorkey((255,255,255))
         
         w, h = size = self.image.get_size()
@@ -387,7 +584,7 @@ class TrashBin(pygame.sprite.Sprite):
     @property
     def top(self):
         return self.image.get_rect()[1] + self.pos[1]
-        
+
 class Amplifier(object):
     def __init__(self, Fs, channels):
         if len(channels) != 2:
@@ -422,21 +619,24 @@ class Amplifier(object):
             print(time.time()-t)
             return s, x
 	
-    def calib(self, t=5):
+    def calib(self, t=5, dt=512//3):
         
         czas_kalibracji = t
    
-        t = time.time()
+        
         l = czas_kalibracji
         self.amp.start_sampling()
-        
-        while time.time() - t < czas_kalibracji + 1:
+        sample = []
+        t = time.time()
+        while time.time() - t <= czas_kalibracji:
             if np.round(time.time() - t,3) == (czas_kalibracji - l) and np.round(time.time() - t,3) < czas_kalibracji + 1:
-                print(l)
+
                 l -= 1
-                
-        scim = self.get_signal(int(czas_kalibracji * self.amp.sampling_rate))
+            sample.append(self.get_signal(dt))        
+    	
         self.amp.stop_sampling()
+
+        scim = np.mean(sample)
         return scim
 
     def get_signal(self, n):
@@ -446,6 +646,7 @@ class Amplifier(object):
         
         s = s[:,self.channels[0]] - s[:,self.channels[1]]
         s-=np.mean(s)
-        s = filtfilt(self.b1,self.a1,s)
+        #fcalibs = filtfilt(self.b1,self.a1,s)
         scim = np.mean(np.abs(s))
         return scim
+
