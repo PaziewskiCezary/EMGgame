2a6dc6b0b0212eb29b2242f8829b228013a53dbb
mpoziomska <mpoziomska@wp.pl>
2022-01-19 13:13:27 +0100

    calibration.py from gui to games

--- emg_games/games/calibration.py
+++ emg_games/games/calibration.py
@@ -14,16 +14,16 @@ class Calibration():
         text_y_position = self.__y_screen // 2
         font_size = self.__y_screen // 10
 
-        self.__screen.fill(color_palette.background_colour)
+        self.__screen.fill(color_palette.BACKGROUND_COLOUR)
         pygame.display.set_caption('Kalibracja')
         self.__update()
 
-        self.__screen.fill(color_palette.background_colour)
+        self.__screen.fill(color_palette.BACKGROUND_COLOUR)
         text(self.__screen, color_palette.text_colour, 'KALIBRACJA', text_x_position, text_y_position, font_size=font_size)
         self.__update()
         time.sleep(2)
 
-        self.__screen.fill(color_palette.background_colour)
+        self.__screen.fill(color_palette.BACKGROUND_COLOUR)
         text(self.__screen, color_palette.text_colour, 'ROZLUŹNIJ RĘKĘ', text_x_position, text_y_position, font_size=font_size)
         self.__update()
         time.sleep(1)
@@ -32,7 +32,7 @@ class Calibration():
 
         time.sleep(2)
 
-        self.__screen.fill(color_palette.background_colour)
+        self.__screen.fill(color_palette.BACKGROUND_COLOUR)
         text(self.__screen, color_palette.text_colour, 'ZACIŚNIJ RĘKĘ', text_x_position, text_y_position, font_size=font_size)
         self.__update()
 
@@ -40,7 +40,7 @@ class Calibration():
 
         self.__calibrate_value_max = self.__get_calib_samples()
         self.__check_calib(text_x_position, text_y_position, font_size)
-        self.__screen.fill(color_palette.background_colour)
+        self.__screen.fill(color_palette.BACKGROUND_COLOUR)
         text(self.__screen, color_palette.text_colour, 'KONIEC KALIBRACJI', text_x_position, text_y_position, font_size=font_size)
         self.__update()
 
@@ -79,7 +79,7 @@ class Calibration():
 
         if self.__calibrate_value_min >= self.__calibrate_value_max or \
                 self.__calibrate_value_max - self.__calibrate_value_min < minimum_difference_between_calibration_values:
-            self.__screen.fill(color_palette.background_colour)
+            self.__screen.fill(color_palette.BACKGROUND_COLOUR)
             text(self.__screen, color_palette.text_colour, 'POWTARZAM KALIBRACJĘ', text_x_position, text_y_position,
                  font_size=font_size)
             self.__update()
