6cafce333a4abede70c9c98c2ecb9dd3872f7cff
adak.crypto <a.kochlewska@student.uw.edu.pl>
2022-01-19 12:50:54 +0100

    good return function

--- games/game_chooser.py
+++ games/game_chooser.py
@@ -20,7 +20,7 @@ text_colour = color_pallet.pink_rgb
 button_colour = color_pallet.pink_rgb
 button_text_colour = color_pallet.yellow_rgb
 
-screen_size = (1600, 900)
+screen_size = (1600 // 2, 900 // 2)
 screen = pygame.display.set_mode(screen_size)
 
 x_screen, y_screen = screen_size
@@ -40,11 +40,20 @@ screen.fill(background_colour)
 
 pygame.display.flip()
 
-list_of_games = ["ŚMIECI", "FIGURY", "KOLORY", "WODA", "ADA", "MARTYNA", "CEZARY", "ŻÓŁW", "DELFIN", "KOT", "ALA MA KOTA"]
+list_of_games = ["ŚMIECI", "FIGURY", "KOLORY", "WODA", "ADA", "MARTYNA", "CEZARY", "ŻÓŁW", "DELFIN", "KOT",
+                 "ALA MA KOTA"]
 
+
+def kill(self):
+    self.__queue.put(1)
+    pygame.quit()
+    exit()
+
+
+# command = lambda l=letter: print_on_press(l)
 
 def get_game_name(index):
-    return list_of_games[index]
+    print(list_of_games[index])
 
 
 def update():
@@ -53,18 +62,12 @@ def update():
 
 number_of_rows = round(math.sqrt(len(list_of_games)))
 number_of_columns = len(list_of_games) // number_of_rows
-print("rows pocz ", number_of_rows)
-print("columns pocz ", number_of_columns)
-print("lista ", len(list_of_games))
+
 if number_of_rows * number_of_columns < len(list_of_games):
     number_of_columns += 1
 
-print("col ", number_of_columns)
-print("rows ", number_of_rows)
 button_width = x_screen / (1.5 * number_of_columns + 0.5)
-# print("weight ", button_weight)
 button_height = y_screen / (1.5 * number_of_rows + 0.5)
-# print("height ", button_height)
 button_dimension = [button_width, button_height]
 font_style = 'Teko'
 
@@ -76,6 +79,7 @@ for game in list_of_games:
         if text_width < button_width:
             final_font_size = font_size
             break
+game_buttons = []
 
 for i in range(number_of_rows):
     for j in range(number_of_columns):
@@ -85,7 +89,17 @@ for i in range(number_of_rows):
         if index >= len(list_of_games):
             break
         game_button = Button(screen, list_of_games[index], [x_position, y_position], button_dimension, button_colour,
-                             button_text_colour, get_game_name(index), final_font_size)
-
+                             button_text_colour, lambda idx=index: get_game_name(idx), final_font_size)
+        game_buttons.append(game_button)
 update()
-time.sleep(20)
+
+while True:
+    for event in pygame.event.get():
+        for game_button in game_buttons:
+            game_button.on_click(event)
+        if event.type == pygame.QUIT:
+            kill()
+        # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_ESCAPE:
+        #         self._menu()
+# time.sleep(20)
