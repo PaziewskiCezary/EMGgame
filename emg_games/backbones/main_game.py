ecb58cf4091df9e2ee4f7a108dad7e654036a532
PaziewskiCezary <czarek9996@gmail.com>
2022-05-20 23:02:49 +0200

    create games once

--- emg_games/backbones/main_game.py
+++ emg_games/backbones/main_game.py
@@ -5,8 +5,11 @@ from emg_games.gui.scenes import choose_game
 
 from emg_games.amplifier import BipolarAmplifier, MonoAmplifier
 
+import emg_games.games
 
-import gc
+
+import inspect
+
 
 
 class MainGame:
@@ -16,6 +19,12 @@ class MainGame:
 		self.screen_properties = ScreenProperties(self.full_screen)
 		self.args = args
 
+		dummy_player = Player(screen_properties=self.screen_properties)
+		self.list_of_games = {obj: obj(full_screen=self.full_screen, player=dummy_player, main_game=self)
+							  for name, obj
+							  in inspect.getmembers(emg_games.games, inspect.isclass)
+							  if issubclass(obj, emg_games.backbones.AbstractGame)}
+
 		self.amp = MonoAmplifier(name=args.amp_name, channels=[24])
 
 		self._new_player()
@@ -24,11 +33,14 @@ class MainGame:
 		
 		game = choose_game(self.screen_properties.screen, screen_properties=self.screen_properties, kill_game=self.player.kill)
 
-		self.game = game(
-						full_screen=self.full_screen,
-						player=self.player, main_game=self)
+		self.game = self.list_of_games[game]
 
-		gc.collect()
+		# self.game = game(
+		# 				full_screen=self.full_screen,
+		# 				player=self.player, main_game=self)
+		#
+		# gc.collect()
+		self.game.player = self.player
 		self.game.menu()
 
 	def _new_player(self):
